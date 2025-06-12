9f88cc17ce20ecd1ab254fefb2d53dc38d76ea6a
Luiz Gasparotto <gasparottoluizhenrique@gmail.com>
2025-06-12 15:47:19 -0300

    commit main.py

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Ou especifique ['http://localhost:3000'] se preferir
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -17,15 +17,15 @@ app.add_middleware(
 
 @app.get("/")
 async def root():
-    return {"message": "Bem-vindo(a) à API do Projeto 3 da CompassUOL!"}
+    return {"message": "Bem-vindo(a) à API do Projeto CICD da CompassUOL!"}
 
-# 1️⃣ Endpoint que retorna uma cor aleatória para mudar a cor da página
+
 @app.get("/color")
 async def get_random_color():
     colors = ["#FF5733", "#33FF57", "#3357FF", "#F333FF", "#33FFF3"]
     return {"color": random.choice(colors)}
 
-# 2️⃣ Endpoint que retorna uma imagem aleatória de gato
+
 @app.get("/cat")
 async def get_random_cat_image():
     async with httpx.AsyncClient() as client:
@@ -36,7 +36,7 @@ async def get_random_cat_image():
             return {"cat_image_url": image_url}
         return JSONResponse(content={"error": "Failed to fetch cat image"}, status_code=500)
 
-# 3️⃣ Endpoint que retorna uma imagem aleatória (via https://picsum.photos)
+
 @app.get("/random-photo")
 async def get_random_photo():
     width = random.randint(200, 600)
@@ -44,13 +44,13 @@ async def get_random_photo():
     photo_url = f"https://picsum.photos/{width}/{height}"
     return {"random_photo_url": photo_url}
 
-# 4️⃣ Endpoint que retorna o horário atual
+
 @app.get("/time")
 async def get_current_time():
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return {"current_time": now}
 
-# 5️⃣ Endpoint que redireciona para uma piada (API pública)
+
 @app.get("/joke")
 async def joke():
     async with httpx.AsyncClient() as client:
@@ -61,17 +61,17 @@ async def joke():
             return {"joke": full_joke}
         return JSONResponse(content={"error": "Failed to fetch joke"}, status_code=500)
 
-#  6️⃣ Retorna uma imagem de susto (ex.: GIF)
+
 @app.get("/scare")
 async def scare():
     scare_images = [
-        "https://media.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif",  # Exemplo de imagem de susto
+        "https://media.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif",  
         "https://media.giphy.com/media/26xBI73gWquCBBCDe/giphy.gif"
     ]
     random_scare = random.choice(scare_images)
     return {"scare_image_url": random_scare}
 
-# 7️⃣ Retorna uma imagem aleatória de “sósia”
+
 @app.get("/lookalike")
 async def lookalike():
     lookalike_images = [
